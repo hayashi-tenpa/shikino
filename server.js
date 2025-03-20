93d9c618bcda54380f7287655d86539440d91999
cark <souryu1010@gmail.com>
2025-03-20 11:08:09 +0900

    change: server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // ミドルウェアの設定
 app.use(express.static(path.join(__dirname, 'public')));
